6699ff7ff04edc3098dfcdd1e1c9d41c28c695d6
Mehwish <mehwishgul715@gmail.com>
2024-04-05 22:26:43 +0500

    commit

--- resources/views/layouts/frontend/include/head.blade.php
+++ resources/views/layouts/frontend/include/head.blade.php
@@ -4,6 +4,9 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<!-- Favicon -->
+<link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon.ico">
+
 <!-- poppins -->
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
       rel="stylesheet">
